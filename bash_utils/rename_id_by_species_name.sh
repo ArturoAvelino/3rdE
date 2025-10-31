0532f9b43659e446f5b8015b9224c6ebfbdec63e
Art Ave <arturo.avelino.h@gmail.com>
2025-10-31 09:48:09 +0100

    Added to the repo some handy bash scripts to process some of the output/input files.

---
+++ bash_utils/rename_id_by_species_name.sh
@@ -0,0 +1,102 @@
+# To run this script, in a terminal go where the files are located
+# and the type:
+#     bash thisfile.sh
+
+find . -type f -name "*.json" -exec sed -i '' 's/\"in87\"/\"Acari\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in88\"/\"Amblypygi\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in89\"/\"Arachnida\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in90\"/\"Araneae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in91\"/\"Astigmata\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in92\"/\"Auchenorrhyncha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in93\"/\"Blattidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in94\"/\"Blattodea\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in95\"/\"Caelifera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in96\"/\"Campodeoidea\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in97\"/\"Cantharidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in98\"/\"Carabidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in99\"/\"Chilopoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in100\"/\"Coleoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in101\"/\"Collembola\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in102\"/\"Corydiidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in103\"/\"Crustacea\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in104\"/\"Curculionidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in105\"/\"Dermaptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in106\"/\"Diplopoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in107\"/\"Diplura\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in108\"/\"Diptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in109\"/\"Dirt\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in110\"/\"Elateridae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in111\"/\"Enchytraeidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in112\"/\"Ensifera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in113\"/\"Entomobryomorpha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in114\"/\"Formicidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in115\"/\"Gastropoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in116\"/\"Geophilomorpha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in117\"/\"Hemiptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in118\"/\"Heteroptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in119\"/\"Heteroptera Herbivores\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in120\"/\"Heteroptera Predators\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in121\"/\"Hexapoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in122\"/\"Hymenoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in123\"/\"Insecta\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in124\"/\"Isopoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in125\"/\"Isoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in126\"/\"Isotomidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in127\"/\"Japygoidea\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in128\"/\"Katiannidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in129\"/\"Lepidoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in130\"/\"Lithobiomorpha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in131\"/\"Lumbricidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in132\"/\"Mesostigmata\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in133\"/\"Metazoa\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in134\"/\"Myriapoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in135\"/\"Neelipleona\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in136\"/\"Oligochaete\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in137\"/\"Onychiuridae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in138\"/\"Opiliones\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in139\"/\"Orthoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in140\"/\"Other Acari\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in141\"/\"Other Arachnida\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in142\"/\"Other Blattodea\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in143\"/\"Other Chilopoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in144\"/\"Other Coleoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in145\"/\"Other Crustacea\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in146\"/\"Other Diplura\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in147\"/\"Other Gastropoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in148\"/\"Other Hemiptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in149\"/\"Other Heteroptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in150\"/\"Other Hymenoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in151\"/\"Other Insecta\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in152\"/\"Other Metazoa\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in153\"/\"Other Myriapoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in154\"/\"Other Orthoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in155\"/\"Pauropoda\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in156\"/\"Poduromorpha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in157\"/\"Prostigmata\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in158\"/\"Protura\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in159\"/\"Pseudoscorpiones\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in160\"/\"Psocoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in161\"/\"Sarcoptiformes\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in162\"/\"Scarabaeidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in163\"/\"Scolopendromorpha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in164\"/\"Scorpiones\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in165\"/\"Silphidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in166\"/\"Sinentomidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in167\"/\"Slugs\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in168\"/\"Sminthurididae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in169\"/\"Snails\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in170\"/\"Solifugae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in171\"/\"Staphylinidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in172\"/\"Sternorrhyncha\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in173\"/\"Symphyla\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in174\"/\"Symphypleona\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in175\"/\"Tenebrionidae\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in176\"/\"Thysanoptera\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in177\"/\"Unclassified\"/g' {} +
+find . -type f -name "*.json" -exec sed -i '' 's/\"in178\"/\"Uropygi\"/g' {} +
+
+# Add the "[" at the begining of the line:
+find . -type f -name "*.json" -exec sed -i '' 's/^/[/' {} +
+
+# Add the "[" at the end of the line:
+find . -type f -name "*.json" -exec sed -i '' 's/$/]/' {} +
